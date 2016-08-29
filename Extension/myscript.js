4108294c4e7cdd2036a26ae4be7be0df9b068396
EbenZhang <zhangyingneng@gmail.com>
2016-08-29 21:53:03 +0800

    Able to add the difftool button when click the files of a pull request.
    github uses pajax to switch tabs (like a single page application) which does not actually request a new webpage.

--- Extension/myscript.js
+++ Extension/myscript.js
@@ -8,7 +8,6 @@ function onDiffToolButtonClicked(){false
             var gitHubToken = items.githubtoken;            
             var toolPath = items.difftoolpath;
         
-            console.log("token" + prUrl);
             chrome.extension.sendMessage(
                 {
                     token: gitHubToken,
@@ -19,7 +18,16 @@ function onDiffToolButtonClicked(){false
     });
 }
 
+function appendDiffToolButton(){
+    if(window.location.href.toLowerCase().indexOf("pull") === -1){
+        return;
+    }
+    $(".file-actions:not(:has(.btn-difftool))").append('<button class="btn-octicon tooltipped tooltipped-nw btn-difftool" rel="nofollow" data-skip-pjax="" aria-label="View the changes in difftool"><svg aria-hidden="true" class="octicon octicon-eye" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg></button>');
+    $(".btn-difftool").off('click', onDiffToolButtonClicked).on('click', onDiffToolButtonClicked);
+}
 $(function(){
-    $(".file-actions").append('<button class="btn-octicon tooltipped tooltipped-nw btn-difftool" rel="nofollow" data-skip-pjax="" aria-label="View the changes in difftool"><svg aria-hidden="true" class="octicon octicon-eye" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg></button>');
-    $(".btn-difftool").click(onDiffToolButtonClicked);
-})
+    $(document).on("pjax:end", function() {
+        appendDiffToolButton();
+    });
+    appendDiffToolButton();
+});
