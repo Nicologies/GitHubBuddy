3505130b32774c6188435d30c03cd07873e4d6fa
Eben Zhang <ebenz@embedcard.com>
2016-09-06 13:41:11 +0800

    Firefox compatibility

--- Host/Dist/install_host.bat
+++ Host/Dist/install_host.bat
@@ -1,1 +1,2 @@
 REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.nicologies.difftool" /ve /t REG_SZ /d "%~dp0com.nicologies.difftool.json" /f
+REG ADD "HKCU\Software\Mozilla\NativeMessagingHosts\com.nicologies.difftool" /ve /t REG_SZ /d "%~dp0com.nicologies.difftool.json" /f
