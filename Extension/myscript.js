e3cf2910fc04e392dbcb790c25fec3849cd8dbd4
EbenZhang <zhangyingneng@gmail.com>
2017-01-01 20:42:27 +0800

    Fixed bug where a repository or user with "pull" in its name will be matched as pull request and thus difftool button added incorrectly.

--- Extension/myscript.js
+++ Extension/myscript.js
@@ -33,7 +33,9 @@ function onDiffToolButtonClicked(){
 }
 
 function appendDiffToolButton(){
-    if(window.location.href.toLowerCase().indexOf("pull") === -1){
+    // location.pathname looks like /owner/repo/pull/id
+    console.log(location.pathname)
+    if (location.pathname.split('/')[3] !== 'pull') {
         return;
     }
     $(".file-actions:not(:has(.btn-difftool))").append('<button class="btn-octicon tooltipped tooltipped-nw btn-difftool" rel="nofollow" data-skip-pjax="" aria-label="View the changes in difftool"><svg aria-hidden="true" class="octicon octicon-eye" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg></button>');
